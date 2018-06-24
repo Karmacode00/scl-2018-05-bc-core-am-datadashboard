c7bd7dd2349843b91fd00aebea4b179e0ff95bf6
AndreaL <alafertteo@gmail.com>
2018-06-23 22:26:46 -0400

    Recorrido con for, variables que necesitamos, pequeñas ediciones al html

--- src/data.js
+++ src/data.js
@@ -1,2 +1,75 @@
+// let usuarios = [];
+window.computeUsersStats = (users, progress) => {
+  let tabla = ''; // Tabla para mostrar los datos
+  let cont = 0; // Contador
+  users.forEach(function(element) {
+    cont++;
+    const progreso = progress[element.id];
+    // Variables para guardar cada dato que se necesita mostrar
+    let lecturaCompletado = 0;
+    let lecturaNoCompletado = 0;
+    let preguntaCompletado = 0;
+    let preguntaNoCompletado = 0;
+    let preguntaPuntaje = 0;
+    let practicaCompletado = 0;
+    let practicaNoCompletado = 0;
+    let contadorPuntaje = 0;
+    // comienzo del ciclo for.. in hasta llegar a lo más profundo del objeto
+    for (let keyCourse in progreso) {
+      let curso = progreso[keyCourse];
+      for (let keyPreUnidad in curso) {
+        let preUnidad = curso[keyPreUnidad];
+        for (let keyUnidad in preUnidad) {
+          let unidad = preUnidad[keyUnidad];
+          for (let keyParte in unidad) {
+            if (keyParte === 'parts') {
+              let parte = unidad[keyParte];
+              for (let keyParteFinal in parte) {
+                let parteFinal = parte[keyParteFinal];
+                switch (parteFinal.type) { // switch para contar la completitud de lecturas, ejercicios y quizzes
+                case 'read': 
+                  if (parteFinal.completed === 1) {
+                    lecturaCompletado++;
+                  } else {
+                    lecturaNoCompletado++;
+                  }
+                  break;
+                case 'quiz':
+                  if (parteFinal.completed === 1) {
+                    preguntaCompletado++;
+                  } else {
+                    preguntaNoCompletado++;
+                  }
+                  if (parteFinal.score === undefined) {
+                    parteFinal.score = 0;
+                  }
+                  preguntaPuntaje += parteFinal.score;
+                  contadorPuntaje++;
+                  break;
+                case 'practice':
+                  if (parteFinal.completed === 1) {
+                    practicaCompletado++;
+                  } else {
+                    practicaNoCompletado++;
+                  }
+                  break;
+                default:
+                  // console.log('holi');
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  });
+};
 
+// Función de ordenado
+window.sortUsers = (users, orderBy, orderDirection) => {
 
+};
+
+// Función de búsqueda
+window.filterUsers = (users, search) => {
+};
--- src/main.js
+++ src/main.js
@@ -1,40 +1,38 @@
 // Generar menú con los cohorts
-const cohortsJSON = '../data/cohorts.json'; // ruta al archivo json
-var data = [];
+const cohortsJSON = '../data/cohorts.json'; // ruta al archivo
+let data = [];
 fetch(cohortsJSON) // acá le digo de donde va  a tomar los datos
-  .then(response => response.json()) // comienza estructra fetch
+  .then(response => response.json())
   .then(data => {
-    // Dibujamos el grupo de cohorts resultante de forEach  
-    var groupCohort = `<label for="selectionCohort">Cohorts</label>
-    <select onchange="selectionCohort()"
-    class="form-control" id="selectionCohort">`; // Evento onchange, damos id
+    // Dibujamos el grupo de cohorts resultante de forEach
+    let grupoCohorts = `<label for='cohorts_select'>Cohorts</label>
+    <select onchange='seleccionarCohorts()' 
+    class='form-control' id='cohortSelect'>`; // Evento onchange, damos id
     data.forEach(function(element) { // Cada iteración va a crear una opción sacando el id
-      groupCohort += '<option value="' + element.id + '">' + element.id + '</option>';
+      grupoCohorts += '<option value="' + element.id + '">' + element.id + '</option>';
     });
-    groupCohort += '</select>'; // Cierro la estructura de select
-    document.getElementById('cohorts').innerHTML = groupCohort; // Imprime en html los cohorts
+    grupoCohorts += '</select>'; // Cierro la estructura de select
+    document.getElementById('cohorts').innerHTML = grupoCohorts;
   });
-  
-// Al seleccionar el cohort que tiene información, muestra esos datos
-function selectionCohort() { // Inicio progress (progreso)
+
+// Al seleccionar el cohort que tiene información, muestra esos datos  
+function seleccionarCohorts() { // Inicio progress (progreso)
   const progressJSON = '../data/cohorts/lim-2018-03-pre-core-pw/progress.json'; // ruta al archivo
-  // var data = []; 
-  fetch(progressJSON) // acá le digo de donde va  a tomar los datos
-    .then(response => response.json()) // Estructura fetch
-    .then(data => {
-      var progress = data;
+  fetch(progressJSON) // acá le digo de donde va  a tomar los datos de progress
+    .then(response => response.json()) // Estructura fetch 
+    .then(data => { // aqui recibo los datos de progreso del JSON
+      const progress = data;
       // Inicio usuarios (users)
       const usersJSON = '../data/cohorts/lim-2018-03-pre-core-pw/users.json'; // ruta al archivo
       var data = [];
-      fetch(usersJSON) // acá le digo de donde va  a tomar los datos
+      fetch(usersJSON) // acá le digo de donde va  a tomar los datos de user
         .then(response => response.json())
-        .then(data => {
+        .then(data => { // aqui recibo los datos de user del JSON
           // Dibujamos el grupo de alumnas
-          const result = data.filter(user => user.signupCohort === document.getElementById('selectionCohort').value);
-          // Se une la función de data js, que entrega datos de completitud
-          computeUsersStats(result, progress, courses);
-          console.log(result)
+          const user = data.filter(user => user.signupCohort === document.getElementById('cohortSelect').value);
+          computeUsersStats(user, progress);
         });
       // Terminado usuarios
     });
 }
+
