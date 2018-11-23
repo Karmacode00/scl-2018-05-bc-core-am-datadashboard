225794a1164517a414f2f7342c042a8ff0f1ddde
AndreaL <alafertteo@gmail.com>
2018-11-22 22:52:31 -0300

    rutas

--- docs/main.js
+++ docs/main.js
@@ -1,5 +1,5 @@
 // Generar menú con los cohorts
-const cohortsJSON = './data/cohorts.json'; // ruta al archivo
+const cohortsJSON = '../data/cohorts.json'; // ruta al archivo
 let data = [];
 fetch(cohortsJSON) // acá le digo de donde va  a tomar los datos
   .then(response => response.json())
@@ -18,13 +18,13 @@ fetch(cohortsJSON) // acá le digo de donde va  a tomar los datos
 
 // Al seleccionar el cohort que tiene información, muestra esos datos  
 function seleccionarCohorts() { // Inicio progress (progreso)
-  const progressJSON = './data/cohorts/lim-2018-03-pre-core-pw/progress.json'; // ruta al archivo
+  const progressJSON = '../data/cohorts/lim-2018-03-pre-core-pw/progress.json'; // ruta al archivo
   fetch(progressJSON) // acá le digo de donde va  a tomar los datos de progress
     .then(response => response.json()) // Estructura fetch 
     .then(data => { // aqui recibo los datos de progreso del JSON
       const progress = data;
       // Inicio usuarios (users)
-      const usersJSON = './data/cohorts/lim-2018-03-pre-core-pw/users.json'; // ruta al archivo
+      const usersJSON = '../data/cohorts/lim-2018-03-pre-core-pw/users.json'; // ruta al archivo
       var data = [];
       fetch(usersJSON) // acá le digo de donde va  a tomar los datos de user
         .then(response => response.json())
